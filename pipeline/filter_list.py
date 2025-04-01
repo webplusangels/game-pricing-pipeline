3e607a6d3daa103eb9882dadaf2298b64b54080d
Hongmin Lee <nimweaver14@gmail.com>
2025-04-01 22:44:04 +0900

    refactor: 리스트 필터링 함수

--- pipeline/filter_list.py
+++ pipeline/filter_list.py
@@ -1,33 +1,41 @@
 from util.io_helper import load_csv, save_csv
+from config import settings
 
-def filter_valid_games(games_list):
+def filter_by_common_ids(games_df, common_ids_path):
     """
-    유효한 정보가 있는 게임만 필터링
+    공통 ID 기준으로 게임 필터링
     """
-    valid_games_df = load_csv(games_list)
-    valid_games_df = valid_games_df[valid_games_df['appid'].notna()]
-    
-    return valid_games_df
+    common_id_df = load_csv(common_ids_path)
+    filtered_games_df = games_df[games_df['appid'].isin(common_id_df['appid'])]
+    return filtered_games_df
 
-def split_free_and_paid(games_list):
+def split_free_and_paid(games_df):
     """
     무료 게임과 유료 게임 분리
     """
-    games_df = load_csv(games_list)
-    is_free_ids_df = games_df[games_df['is_free'] == True]
-    is_paid_ids_df = games_df[games_df['is_free'] == False]
+    free_games_df = games_df[games_df['is_free'] == True]
+    paid_games_df = games_df[games_df['is_free'] == False]
+    return free_games_df, paid_games_df
 
-    return is_free_ids_df, is_paid_ids_df
-
-def filter_games(games_list):
+def filter_games(games_list_path):
     """
-    게임 리스트 필터링
+    게임 리스트 필터링 및 저장
     """
-    valid_games_df = filter_valid_games(games_list)
-    save_csv(valid_games_df, games_list)
+    # 게임 리스트 로드
+    games_df = load_csv(games_list_path)
     
-    free_steam_list, paid_steam_list = split_free_and_paid(games_list)
-    save_csv(free_steam_list, "./data/raw/free_steam_list.csv")
-    save_csv(paid_steam_list, "./data/raw/paid_steam_list.csv")
+    # 유효한 appid 필터링
+    games_df = games_df[games_df['appid'].notna()]
     
-    return
+    # 공통 ID 기준으로 필터링
+    games_df = filter_by_common_ids(games_df, settings.LIST_DIR)
+    
+    # 필터링된 게임 리스트 저장
+    save_csv(games_df, games_list_path)
+    
+    # 무료 및 유료 게임 분리
+    free_games_df, paid_games_df = split_free_and_paid(games_df)
+    
+    # 무료 및 유료 게임 저장
+    save_csv(free_games_df, "./data/raw/free_steam_list.csv")
+    save_csv(paid_games_df, "./data/raw/paid_steam_list.csv")
