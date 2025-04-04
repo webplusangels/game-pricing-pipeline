d9a39f05525e0cf58ee4ca929b913c609f25e9e9
Hongmin Lee <nimweaver14@gmail.com>
2025-04-04 09:54:05 +0900

    fix: 파이프라인 구성

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from pipeline.fetch_steam_list import SteamListFetcher
 from pipeline.fetch_steam_detail import SteamDetailFetcher
 from pipeline.fetch_steam_review import SteamReviewFetcher
 from pipeline.fetch_steam_active_player import SteamActivePlayerFetcher
-# from pipeline.fetch_itad_id import ITADIdFetcher
-# from pipeline.fetch_itad_price import ITADPriceFetcher
-# from pipeline.process_data import DataProcessor
-# from pipeline.save_to_rds import RDSUploader
+from pipeline.fetch_itad_id import ITADIdFetcher
+from pipeline.fetch_itad_price import ITADPriceFetcher
+from pipeline.backup_tables import backup_tables_to_csv
+from pipeline.process_data import DataProcessor
+from pipeline.save_to_db import DBUploader
 from concurrent.futures import ThreadPoolExecutor
 from config import settings
 from time import time
@@ -17,6 +18,8 @@ from pipeline.filter_list import filter_games
 def main():
     steam_key = settings.STEAM_KEY
     list_dir = settings.LIST_DIR
+    paid_game_dir = settings.PAID_LIST_DIR
+    free_game_dir = settings.FREE_LIST_DIR
     logger = setup_logger(
         name="main_pipeline", 
         log_dir="log/main",
@@ -38,7 +41,7 @@ def main():
     with log_step("Steam 게임 리스트 가져오기", logger):
         steam_list_fetcher = SteamListFetcher(
             webapi_key=steam_key,
-            steamcharts_games=500,
+            steamcharts_games=3000, # 테스트용 300개
             )
         steam_list_fetcher.run()
         logger.info("Steam 게임 리스트 가져오기 완료")
@@ -51,6 +54,7 @@ def main():
         
     # 필터링 하기, 무료/유료 게임 리스트 분리
     filter_games('./data/raw/steam_game_detail_parsed.csv')
+    logger.info("Steam 게임 리스트 필터링 완료")
  
     with log_step("Steam 게임 리뷰 및 활성 플레이어 정보 병렬 가져오기", logger):
         review_fetcher = SteamReviewFetcher()
@@ -72,35 +76,36 @@ def main():
             except Exception as e:
                 logger.error(f"활성 플레이어 수집 중 오류 발생: {e}")
             
-    # step_start_time = time()
-    # itad_id_fetcher = ITADIdFetcher(settings.ITAD_KEY)
-    # itad_data = itad_id_fetcher.fetch_all(steam_list)
-    # step_end_time = time()
-    # logger.info(f"ITAD ID 데이터 가져오기 완료: {step_end_time - step_start_time:.2f}초 소요")
+    with log_step("ITAD ID 데이터 가져오기", logger):
+        itad_id_fetcher = ITADIdFetcher()
+        itad_id_fetcher.run(paid_game_dir)
+        logger.info("ITAD ID 데이터 가져오기 완료")
     
-    # step_start_time = time()
-    # itad_price_fetcher = ITADPriceFetcher(settings.ITAD_KEY)
-    # itad_data = itad_price_fetcher.fetch_all(steam_list)
-    # step_end_time = time()
-    # logger.info(f"ITAD 가격 데이터 가져오기 완료: {step_end_time - step_start_time:.2f}초 소요")
+    with log_step("ITAD 가격 데이터 가져오기", logger):
+        itad_price_fetcher = ITADPriceFetcher()
+        itad_price_fetcher.run()
+        logger.info("ITAD 가격 데이터 가져오기 완료")
+    
+    backup_tables_to_csv([
+        "category",
+        "platform",
+        "game_static",
+        "game_dynamic",
+        "game_category",
+        "current_price_by_platform",
+    ])
+    
+    # 3. 데이터 처리
+    with log_step("데이터 처리", logger):
+        data_processor = DataProcessor()
+        data_processor.run()
+        logger.info("데이터 처리 완료") 
 
-    # # 3. 데이터 처리
-    # with log_step("데이터 처리", logger):
-    #     processor = DataProcessor()
-    #     processed_data = processor.merge(
-    #         steam_detail, 
-    #         steam_review, 
-    #         steam_active_player, 
-    #         # itad_data
-    #     )
-    #     logger.info("데이터 처리 완료") 
-
-    # # 4. 데이터베이스 업로드
-    # with log_step("데이터베이스 업로드", logger):
-    #     step_start_time = time()
-    #     uploader = RDSUploader(settings.DB_URL)
-    #     uploader.save(processed_data)
-    #     logger.info("데이터베이스 업로드 완료")
+    # 4. 데이터베이스 업로드
+    with log_step("데이터베이스 업로드", logger):
+        db_uploader = DBUploader()
+        db_uploader.run()
+        logger.info("데이터베이스 업로드 완료")
 
 if __name__ == "__main__":
     main()
